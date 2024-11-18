ef2935d1a606f6ef383a179ea928fb2254b3d41e
Yunyung <yungyung7654321@gmail.com>
2024-11-19 02:38:46 +0800

    HW1: Increase batch size

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -161,7 +161,9 @@ public class SendYourData {
                   ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                   bootstrapServers,
                   ProducerConfig.LINGER_MS_CONFIG,
-                  "5000"),
+                  "5000",
+                  ProducerConfig.BATCH_SIZE_CONFIG,
+                  "65536"),
               serializer,
               new ByteArraySerializer());
     }
