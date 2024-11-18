0abe3413422985b2cf65a1485ce2cc5c47e323b5
Yunyung <yungyung7654321@gmail.com>
2024-11-19 03:20:54 +0800

    hw1: LINGER_MS 5000

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -169,7 +169,7 @@ public class SendYourData {
                   ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                   bootstrapServers,
                   ProducerConfig.LINGER_MS_CONFIG,
-                  "2000",
+                  "5000",
                   ProducerConfig.BATCH_SIZE_CONFIG,
                   "65536"),
               serializer,
